bab8614497d06ed9bdad7fc48fc64d154b477196
Guillaume Vaudet <guillaumevaudet.pro@gmail.com>
2026-03-28 17:28:31 +0100

    feat: add _printf file and include it into main header

---
+++ _printf.c
@@ -0,0 +1,11 @@
+/**
+* _printf - display a string according to a format
+* @format: pointer to a string
+* Return: int - numbers of char printed
+*/
+int _printf(const char *format, ...)
+{
+	int counter;
+
+	return (counter);
+}
