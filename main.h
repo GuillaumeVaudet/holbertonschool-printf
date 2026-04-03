8dc06a73415d03bfc6bf59345690f900e12abf9f
Sébastien Lamblin <seb.lamblin59@gmail.com>
2026-04-03 10:26:30 +0200

    fix: add prototypes to the main header

--- main.h
+++ main.h
@@ -3,6 +3,11 @@
 
 #include <stdarg.h>
 
+/**
+* specifier - new struct to print
+* @id: char
+* @f: pointer to a func
+*/
 typedef struct
 {
 	char id;
@@ -11,4 +16,9 @@ typedef struct
 
 int _putchar(char c);
 int _printf(const char *format, ...);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(va_list args);
+
 #endif
+
