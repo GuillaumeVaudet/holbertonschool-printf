a90879cc199468036cbe0a7d34a0fe979b59e943
Guillaume Vaudet <guillaumevaudet.pro@gmail.com>
2026-04-04 22:31:04 +0200

    refacto: refacto printf file to have less lines

--- _printf.c
+++ _printf.c
@@ -1,6 +1,28 @@
 #include <stdlib.h>
-#include <stdarg.h>
 #include "main.h"
+
+/**
+ * get_specifier - searches for a matching specifier in the array
+ * @c: char to compare with array
+ * @array: array that use to compare and return the correct function
+ * @size: size of the array
+ * Return: pointer to the matching function, or NULL if not found
+ */
+int (*get_specifier(char c, specifier_t *array, int size))(va_list)
+{
+	int i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (c == array[i].id)
+		{
+			return (array[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
 /**
 * _printf - display a string according to a format
 * @format: pointer to a string
@@ -9,8 +31,9 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
+	int (*f)(va_list);
 
-	int i = 0, j, counter = 0;
+	int i = 0, counter = 0;
 
 	specifier_t array_of_specifier[5] = {
 		{'c', print_char},
@@ -33,18 +56,12 @@ int _printf(const char *format, ...)
 		{
 			i++;
 			if (format[i] == '\0')
-				return (counter);
-			j = 0;
-			while (j < 5)
+				return (-1);
+			f = get_specifier(format[i], array_of_specifier, 5);
+			if (f != NULL)
 			{
-				if (format[i] == array_of_specifier[j].id)
-				{
-					counter += array_of_specifier[j].f(args);
-					break;
-				}
-				j++;
-			}
-			if (j == 5)
+				counter += f(args);
+			} else
 			{
 				_putchar(format[i]);
 				counter++;
